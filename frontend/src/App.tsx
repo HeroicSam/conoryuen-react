1be8fd09d2d087b11f4c15ac3e89f91689d5e8ec
HeroicSam <cyuen10@gmail.com>
2023-01-12 09:07:46 -0500

    fix: tied parallax to mousemove. camera previously stopped working when adding other dom elements over the canvas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { Canvas, } from '@react-three/fiber';
 import Leaf from './components/Leaf';
 import ParallaxCamera from './components/ParallaxCamera';
@@ -7,18 +7,23 @@ import { EffectComposer, DepthOfField } from '@react-three/postprocessing';
 
 function App({ count = 80, depth = 80 }) {
   return (
-    <Canvas gl={{ alpha: false}} camera={{near: 0.01, far: 110, fov: 20 }} >
-      <color attach="background" args={["#ffbf40"]} />
-      <spotLight position={[10, 10, 10]} intensity={1} />
-      <Suspense fallback={null}>
-        <Environment preset='apartment' />
-        {Array.from({ length: count }, (_, i) => (<Leaf key={i} z={-(i / count) * depth - 20} />))}
-        <EffectComposer>
-          <DepthOfField target={[0, 0, depth / 2]} focalLength={0.5} bokehScale={11} height={700} />
-        </EffectComposer>
-        <ParallaxCamera />
-      </Suspense>
-    </Canvas>
+    <>
+      <div className="overlay">
+        <h1>hello</h1>
+      </div>
+      <Canvas gl={{ alpha: false}} camera={{near: 0.01, far: 110, fov: 20 }} >
+        <color attach="background" args={["#ffbf40"]} />
+        <spotLight position={[10, 10, 10]} intensity={1} />
+        <Suspense fallback={null}>
+          <Environment preset='apartment' />
+          {Array.from({ length: count }, (_, i) => (<Leaf key={i} z={-(i / count) * depth - 20} />))}
+          <EffectComposer>
+            <DepthOfField target={[0, 0, depth / 2]} focalLength={0.5} bokehScale={11} height={700} />
+          </EffectComposer>
+          <ParallaxCamera />
+        </Suspense>
+      </Canvas>
+    </>
   )
 }
 
--- frontend/src/components/ParallaxCamera.tsx
+++ frontend/src/components/ParallaxCamera.tsx
@@ -1,11 +1,28 @@
 import * as THREE from 'three';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { PerspectiveCamera } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 
 export default function ParallaxCamera() {
   const [vec] = useState(() => new THREE.Vector3())
-  const { camera, mouse } = useThree()
-  useFrame(() => camera.position.lerp(vec.set(mouse.x * 0.5, 1, 0.5), 0.05))
+  const { camera } = useThree()
+  const cursorPos = { x: 0, y: 0 };
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      cursorPos.x = e.clientX
+      cursorPos.y = e.clientY
+    }
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    }
+  }, [])
+
+  useFrame(() => {
+    camera.position.lerp(vec.set(cursorPos.x * 0.0005, cursorPos.y * 0.0005, 0.5), 0.05)
+  })
   return <PerspectiveCamera />
 }
