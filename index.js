7757908db18295225c1a8da6ecbf5888b30c9c33
HeroicSam <cyuen10@gmail.com>
2023-01-17 20:34:42 -0500

    fixed heroku possibly

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const express = require("express");
 const app = express();
 const port = process.env.NODE_ENV || 5175;
 
-app.use(express.static('/', './frontend/dist'));
+app.use(express.static('./frontend/dist'));
 app.listen(port, () => console.log('App is running on port 5175'));
