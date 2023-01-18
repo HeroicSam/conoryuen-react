ddf1db796c80ec7ee92da075f6c227b80e925101
HeroicSam <cyuen10@gmail.com>
2023-01-17 20:49:43 -0500

    changed port var

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
-const port = process.env.NODE_ENV || 5175;
+const port = process.env.PORT || 5175;
 
 app.use(express.static('./frontend/dist'));
 app.listen(port, () => console.log('App is running on port 5175'));
