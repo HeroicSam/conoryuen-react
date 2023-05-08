b5c6d482eb6e60eb3045cfb30d7f6e6df0db4a6f
HeroicSam <cyuen10@gmail.com>
2023-05-07 21:11:59 -0400

    tesing noise filter, adding rotating blobs for loading screen

--- frontend/src/components/LoadingScreen.jsx
+++ frontend/src/components/LoadingScreen.jsx
@@ -12,14 +12,39 @@ function LoadingScreen() {
 
 
   return (
-    <div className="absolute w-full h-full bg-gradient-to-br from-soft-yellow to-soft-green">
-      <h1 className="text-black">
-        { loadProgress }%
-      </h1>
-    </div>
+    <>
+      <div
+        className="loadingScreen absolute w-full h-full flex justify-end items-end text-8xl font-migra"
+      >
+        <h1 className="text-black font-bold mr-8 mb-8">
+          { loadProgress }%
+        </h1>
+        <div className="absolute w-full h-full blob-cont">
+          <div className="absolute rounded-full w-16 h-16 bg-black" />
+          <div className="absolute rounded-full bg-yellow"/>
+        </div>
+      </div>
+      <svg>
+        {/* <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="100%">
+          <stop offset="0%" stop-color="#FBED96" />
+          <stop offset="100%" stop-color="#ABECD6" />
+        </linearGradient> */}
+        <filter id='noiseFilter'>
+          <feTurbulence 
+            type='fractalNoise' 
+            baseFrequency='.6' 
+            stitchTiles='stitch'/>
+          <feColorMatrix in="colorNoise" type="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 1 0" />
+          <feComposite operator="in" in2="SourceGraphic" result="monoNoise"/>
+          <feBlend in="SourceGraphic" in2="monoNoise" mode="screen" />
+        </filter>
+      </svg>
+    </>
   )
 }
 
 export default LoadingScreen;
 
 // need to add a svg noise filter and animate the gradient clockwise
+
+// bg-gradient-to-br from-soft-yellow to-soft-green
