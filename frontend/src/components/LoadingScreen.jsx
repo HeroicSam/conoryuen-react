cece261b155702a7a1aeb54313854e2e57d5dcf2
HeroicSam <cyuen10@gmail.com>
2023-05-16 21:05:09 -0400

    refactored loading screen, testing new mask effect

--- frontend/src/components/LoadingScreen.jsx
+++ frontend/src/components/LoadingScreen.jsx
@@ -1,32 +1,41 @@
 import { useState, useEffect, useRef } from "react";
-import gsap from 'gsap';
+import gsap, { Power0 } from 'gsap';
 
 function LoadingScreen() {
 
   const [loadProgress, setLoadProgress] = useState(0);
   const [loading, setLoading] = useState(true);
   const blobContainerRef = useRef(null);
+  const blobRef = useRef(null);
   let width = window.innerWidth;
   let height = window.innerHeight;
   let blobContainerWidth;
   let blobContainerHeight;
 
+  const t1 = gsap.timeline()
+
   window.addEventListener('resize', () => {
+
     width = window.innerWidth;
     height = window.innerHeight;
+
     if (!loading) {
       endLoadingScreen();
     }
+
   })
 
   function endLoadingScreen(){
-    t1.pause();
+
+    // t1.pause();
+
     gsap.to(".loadingScreen", {
       width: width * 0.35,
       height: width * 0.35,
       borderRadius: "50%",
       duration: 2,
     })
+
     gsap.to(".overlay", {
       width: width * 0.35,
       height: width * 0.35,
@@ -35,95 +44,81 @@ function LoadingScreen() {
       duration: 2,
 
     })
+
     gsap.to(".blob-cont", {
       borderRadius: "50%",
       duration: 2,
     })
-    // gsap.set(".blob", {
-    //   width: width * 1.25,
-    //   height: width * 1.25,
-    //   top: 0 - ( 1.3 * height ),
-    //   left: 0 - (.7 * width)
-    // })
+
   }
 
-  const t1 = gsap.timeline()
-
   function animateBlob(){
-    //   t1.to(".blob", {
-    //     left: width * .2,
-    //     scale: 1.3,
-    //     duration: 4,
-    //   }).to(".blob", {
-    //     top: height * .2,
-    //     scale: 0.9,
-    //     duration: 3.5,
-    //   }).to (".blob", {
-    //     left: 0 - (.7 * width),
-    //     scale: 1.1,
-    //     duration: 4,
-    //   }).to(".blob", {
-    //     top: 0 - ( 1.3 * height ),
-    //     scale: 1.0,
-    //     duration: 3,
-    // }).repeat(-1)
-    t1.to(".blob", {
-      rotation: 360,
-      duration: 5,
-      repeat: -1,
-    })
+
+    // t1.to(".blob", {
+    //   rotation: 360,
+    //   duration: 5,
+    //   ease: Power0.easeNone,
+    //   repeat: -1,
+    // })
+
   }
 
 
   setTimeout(() => {
+
     if (loadProgress < 100) {
       setLoadProgress(loadProgress + 1);
     } else {
-
-      endLoadingScreen();
-
       setTimeout(()=> {
         setLoading(false);
-        endLoadingScreen();
+        // endLoadingScreen();
       }, 1000)
     }
+
   }, 50)
 
   useEffect(() => {
 
-    // gsap.set(".blob", {
-    //   width: width * 1.25,
-    //   height: width * 1.25,
-    //   top: 0 - ( 1.3 * height ),
-    //   left: 0 - (.7 * width)
-    // })
-    console.log(blobContainerRef.current.clientHeight);
-
     gsap.set(".blob", {
-      width: 500,
-      height: 500,
-      top: 0 - ( 1.3 * height ),
-      left: 0 - (.7 * width)
+      width: "1000px",
+      height: "1000px",
     })
-    animateBlob();
+
+    // animateBlob();
+
+    const handleMouseMove = (e) => {
+      gsap.to(".blob", {
+        x: e.clientX,
+        y: e.clientY,
+        duration: 2,
+      })
+    }
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    }
 
   },[])
 
 
   return (
     <>
-      <div className="overlay flex justify-end items-end overflow-hidden">
+      <div className="overlay absolute w-full h-full flex justify-end items-end overflow-hidden z-20">
         <h1 className="loadProgress font-migra text-black font-bold text-8xl mr-8 mb-8 z-30">
           { loading ? `${loadProgress} %` : null }
         </h1>
       </div>
-      <div
-        className="loadingScreen absolute w-full h-full flex justify-end items-end overflow-hidden"
-      >
-        <div ref={blobContainerRef} className="blob-cont absolute w-full h-full bg-soft-green">
-          <div className="blob absolute rounded-full bg-soft-yellow" />
-        </div>
+      <div className="loadingScreen absolute w-full h-full bg-soft-green overflow-hidden">
+        <div ref={blobRef} className="blob absolute rounded-full bg-soft-yellow translate-x-[-50%] translate-y-[-50%]" />
+        <div className="hole"></div>
       </div>
+      <div className="parent absolute h-full w-full bg-white flex justify-center items-center">
+        <h1>We are farmersddddddddddddd</h1>
+      </div>
+      {/* <div className="grandparent absolute w-full h-full bg-transparent flex justify-center items-center">
+      </div> */}
       <svg>
         <filter id='noiseFilter'>
           <feTurbulence 
