8903b5a38f7c471c77172c2473125524e3da925f
HeroicSam <cyuen10@gmail.com>
2023-05-22 22:02:50 -0400

    added more to animation, figuring out a hacky solution, need to add calculations and resizing handlers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,8 @@ import LoadingScreen from "./components/LoadingScreen";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 
-import registerEffects from "./utility/RegisterEffects";
-
 function App() {
 
-  registerEffects();
-  
   return (
     <div className="flex justify-center items-center font-mori h-full w-full">
       <NavBar />
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,43 +1,85 @@
 import gsap from "gsap";
-import { useEffect, useRef } from "react";
+import { useLayoutEffect, useRef } from "react";
 
 function Home() {
 
-  const tickerWrapperRef = useRef(null);
-  let tickerWidth, spanWidth;
-  const tickerItemsRef = useRef(null);
+  const tickerWrapperRef = useRef();
+  const tickerItemRef = useRef();
+  const tickerItemRefTwo = useRef();
 
-  const t1 = gsap.timeline({ repeat: -1 })
-  t1.to("li", {
-    duration: 1,
-    x: "-=50",
-    ease: "Power0.easeNone",
-  })
-  t1.play()
+  let t1 = gsap.timeline();
+  let t2 = gsap.timeline();
 
-  useEffect(() => {
+  useLayoutEffect(() => {
 
-    const tickerWrapper = tickerWrapperRef.current;
-    const tickerItems = tickerItemsRef.current;
-    const elem = document.querySelector(".ticker-wrapper")
-    const tickerWidth = elem.getBoundingClientRect().width;
+    let tickerWrapperWidth = tickerWrapperRef.current.offsetWidth;
 
-    
-    
+    let spanWidth = tickerItemRef.current.offsetWidth
+    let speed = 100;
+
+
+    t1.fromTo(tickerItemRef.current, {
+      xPercent: 100,
+    }, {
+      xPercent: 0,
+      duration: 2,
+      ease: "none"
+    }).to(tickerItemRef.current, {
+      xPercent: - (spanWidth / tickerWrapperWidth) * 100,
+      ease: "none",
+      duration: 2,
+    }).repeat(-1)
+
+    t2.fromTo(tickerItemRefTwo.current, {
+      xPercent: 100,
+    }, {
+      xPercent: 0,
+      duration: 2,
+      ease: "none"
+    }).to(tickerItemRefTwo.current, {
+      xPercent: - (spanWidth / tickerWrapperWidth) * 100,
+      ease: "none",
+      duration: 2,
+    }).repeat(-1).delay(2)
+
   }, [])
+
+  function handleMouseEnter() {
+    gsap.to(t1, {
+      timeScale: 0,
+      overWrite: true
+    })
+
+    gsap.to(t2, {
+      timeScale: 0,
+      overWrite: true
+    })
+  }
+
+  function handleMouseOut() {
+    gsap.to(t1, {
+      timeScale: 1,
+      overWrite: true
+    })
+
+    gsap.to(t2, {
+      timeScale: 1,
+      overWrite: true
+    })
+  }
 
   return(
     
-    <div className="absolute w-full h-full font-migra font-bold italic whitespace-nowrap inline-block">
-      <ul className="absolute text-[15rem]">
-        <span className="ticker-wrapper">
-          <span className="ticker-items">
-            <li className="">Welcome to my Folio</li>
-          </span>
-        </span>
+    <div className="absolute w-full h-full font-migra font-bold italic whitespace-nowrap inline-block overflow-hidden">
+      <ul onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut} ref={tickerWrapperRef} className="ticker-wrapper absolute text-[15rem] w-full whitespace-nowrap inline-block ">
+          <li ref={tickerItemRef} className="ticker-item absolute">Welcome to my Folio</li>
+          <li ref={tickerItemRefTwo} className="ticker-item absolute">Welcome to my Folio</li>
       </ul>
     </div>
   )
 }
 
 export default Home;
+
+// just as the end of Folio enters the screen, the animation needs to replay, reusing the text and makes the loop seamless
+// need to tie animation to element dimensions
