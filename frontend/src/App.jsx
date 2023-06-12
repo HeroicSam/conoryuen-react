127f37d486d94c6b89f42dae9bef09fa5f19137e
HeroicSam <cyuen10@gmail.com>
2023-06-11 21:51:36 -0400

    added another ticker

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,67 +17,69 @@ function App() {
 
   const sizes = useViewportDimensions();
 
-  let tickerItems;
+  const tickerLoops = useRef([]);
+  const tickerLoopsTwo = useRef([]);
+
+  let tickerItems, tickerItemsTwo;
 
   useLayoutEffect(() => {
 
-    loading && setTimeout(() => {
-
-      gsap.set(".ticker-wrapper", {
-        y: window.innerHeight / 4,
-      })
-
-    }, 500)
-
     tickerItems = gsap.utils.toArray(".tickerItem")
-    console.log(tickerItems)
-    const loop = horizontalLoop(tickerItems, {paused: false, speed: 3, repeat: -1});
+    tickerItemsTwo = gsap.utils.toArray(".tickerItemTwo")
 
-  }, [])
+    tickerLoops.current = horizontalLoop(tickerItems, {paused: false, speed: 3, repeat: -1});
+    tickerLoopsTwo.current = horizontalLoop(tickerItemsTwo, {paused: false, speed: 3, repeat: -1, reversed: 1});
+
+    loading && gsap.set(".ticker-wrapper", {
+      y: window.innerHeight / 4,
+    })
+
+  }, []);
 
   function textTransition() {
     gsap.to(".ticker-wrapper", {
       y: 0,
       ease: "Power4.easeOut",
       delay: 0.1,
-      duration: 1.8
-    })
-  }
+      duration: 1.8,
+      stagger: 0.2,
+    });
+  };
 
   function handleMouseEnter() {
     if (enableHoverAnimations) {
       gsap.to(t1, {
         timeScale: 1,
-      })
+      });
       gsap.to(t2, {
         timeScale: 1,
-      })
-    }
-  }
+      });
+    };
+  };
 
   function handleMouseOut() {
     if (enableHoverAnimations) {
       gsap.to(t1, {
         timeScale: 0,
-      })
+      });
   
       gsap.to(t2, {
         timeScale: 0,
-      })
-    }
-  }
+      });
+    };
+  };
   
   function stopAnimations() {
     gsap.to(t1, {
       timeScale: 0,
       overWrite: true,
-    })
+    });
     gsap.to(t2, {
       timeScale: 0,
       overWrite: true,
-    })
+    });
     setEnableHoverAnimations(true);
-  }
+  };
   
   return (
     <div className="flex justify-center items-center font-mori h-full w-full">
@@ -89,14 +91,21 @@ function App() {
         textTransition={textTransition}
         stopAnimations={stopAnimations}
       />
-      <div className="absolute w-full h-full font-migra font-bold italic whitespace-nowrap inline-block overflow-x-hidden">
-        <div className="ticker text-[20vh] md:text-[18vh] w-full h-80 whitespace-nowrap inline-block mt-28 overflow-x-hidden leading-loose">
+      <div className="absolute flex flex-col w-full h-full font-migra font-bold italic whitespace-nowrap inline-block overflow-x-hidden">
+        <div className="ticker text-[20vh] md:text-[18vh] w-full h-64 whitespace-nowrap inline-block mt-40 overflow-x-hidden leading-none">
           <div className="ticker-wrapper w-full relative flex">
             <div className="tickerItem">Welcome to my Folio</div>
             <div className="tickerItem">Welcome to my Folio</div>
             <div className="tickerItem">Welcome to my Folio</div>
           </div>
         </div>
+        <div className="ticker text-[20vh] md:text-[18vh] w-full h-64 whitespace-nowrap inline-block overflow-x-hidden leading-none z-50">
+        <div className="ticker-wrapper w-full relative flex">
+          <div className="tickerItemTwo">Welcome to my Folio</div>
+          <div className="tickerItemTwo">Welcome to my Folio</div>
+          <div className="tickerItemTwo">Welcome to my Folio</div>
+        </div>
+      </div>
       </div>
     </div>
   )
